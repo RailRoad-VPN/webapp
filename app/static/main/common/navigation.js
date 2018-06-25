73447f8fe1ace296b81f4a8e00b86010e48a9331
Artur Karapetov <858683@gmail.com>
2018-06-25 18:54:08 +0300

    fix nav scroll

--- app/static/main/common/navigation.js
+++ app/static/main/common/navigation.js
@@ -1,5 +1,36 @@
 'use strict';
 
 $(document).ready(function () {
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#main-navbar',
+        offset: 54
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#main-navbar").offset().top > 100) {
+            $("#main-navbar").addClass("navbar-shrink");
+        } else {
+            $("#main-navbar").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
 });
