3bc3595dfc077ad077fd5056633e2dc5b7915542
Artur Karapetov <858683@gmail.com>
2018-07-04 15:14:21 +0300

    fix translation, fix errors, fix pep8

--- app/mods/index/controller.py
+++ app/mods/index/controller.py
@@ -2,11 +2,10 @@ import logging
 import sys
 from http import HTTPStatus
 
-import datetime
-from flask import Blueprint, render_template, session, request, redirect, abort, jsonify
+from flask import Blueprint, render_template, session, request, jsonify
 
 # Define the blueprint: 'index', set its url prefix: app.url/
-from app import rrn_billing_service, app_config, email_service, EmailMessageType, EmailMessage
+from app import rrn_billing_service, app_config, email_service
 from app.flask_utils import _pull_lang_code, _add_language_code
 from app.models import AjaxResponse, AjaxError
 
@@ -31,7 +30,7 @@ def pull_lang_code(endpoint, values):
 @mod_index.route('/', methods=['GET'])
 def index_lang_page():
     logger.info('index_lang page')
-    
+
     try:
         subscriptions = rrn_billing_service.get_subscriptions(lang_code=session['lang_code'])
     except APIException:
